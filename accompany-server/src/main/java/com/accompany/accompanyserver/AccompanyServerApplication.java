296b55ba90034cf8494957c3b5b47790eb4c9add
jackson-ling <1524205121@qq.com>
2025-11-15 15:32:36 +0800

    项目搭建完成，依赖管理待实现

---
+++ accompany-server/src/main/java/com/accompany/accompanyserver/AccompanyServerApplication.java
@@ -0,0 +1,13 @@
+package com.accompany.accompanyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccompanyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccompanyServerApplication.class, args);
+    }
+
+}
